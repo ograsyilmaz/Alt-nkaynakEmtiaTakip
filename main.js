c3405b011abc0e473b159d50344847fd43d82231
ograsyilmaz <yilmazogras@outlook.com.tr>
2020-02-24 13:53:28 +0300

    user pasword

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ var soap ='<?xml version="1.0" encoding="utf-8"?>'+
 '<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">'+
   '<soap12:Header>'+
    ' <AuthHeader xmlns="http://data.altinkaynak.com/">'+
-      '<Username>'+'</Username>'+
-      '<Password>'+'</Password>'+
+      '<Username>'+username+'</Username>'+
+      '<Password>'+password+'</Password>'+
     '</AuthHeader>'+
   '</soap12:Header>'+
   '<soap12:Body>'+
