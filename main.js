e0282fd109bf1eaf084a1f2aba9c18a07b1c6e9d
ograsyilmaz <yilmazogras@outlook.com.tr>
2020-02-24 14:48:10 +0300

    test1

--- main.js
+++ main.js
@@ -5,53 +5,54 @@ var username = "AltinkaynakWebServis";
 var password = "AltinkaynakWebServis";
 var requesDataMethod = "POST";
 var Url = "https://data.altinkaynak.com/DataService.asmx";
-//var soapActionHeader = "http://data.altinkaynak.com/GetGold";
-// var soap = '<?xml version="1.0" encoding="utf-8"?>' +
-//     '<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">' +
-//     ' <soap12:Header>' +
-//     '<AuthHeader xmlns="http://data.altinkaynak.com/">' +
-//     '<Username>' + username + '</Username>' +
-//     '<Password>' + password + '</Password>' +
-//     '</AuthHeader>' +
-//     '</soap12:Header>' +
-//     ' <soap12:Body>' +
-//     '<GetGold xmlns="https://data.altinkaynak.com/" />' +
-//     '</soap12:Body>' +
-//     '</soap12:Envelope>';
+var soapActionHeader = "http://data.altinkaynak.com/GetGold";
+var soap = '<?xml version="1.0" encoding="utf-8"?>' +
+    '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+    ' <soap:Header>' +
+    '<AuthHeader xmlns="http://data.altinkaynak.com/">' +
+    '<Username>' + username + '</Username>' +
+    '<Password>' + password + '</Password>' +
+    '</AuthHeader>' +
+    ' </soap:Header>' +
+    ' <soap:Body>' +
+    '<GetGold xmlns="http://data.altinkaynak.com/" />' +
+    ' </soap:Body>' +
+    '</soap:Envelope>';
 
 
-var soap ='<?xml version="1.0" encoding="utf-8"?>'+
-'<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">'+
-  '<soap12:Header>'+
-   ' <AuthHeader xmlns="http://data.altinkaynak.com/">'+
-      '<Username>'+username+'</Username>'+
-      '<Password>'+password+'</Password>'+
-    '</AuthHeader>'+
-  '</soap12:Header>'+
-  '<soap12:Body>'+
-   '<GetGold xmlns="http://data.altinkaynak.com/" />'+
-  '</soap12:Body>'+
-'</soap12:Envelope>';
+// var soap ='<?xml version="1.0" encoding="utf-8"?>'+
+// '<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">'+
+//   '<soap12:Header>'+
+//    ' <AuthHeader xmlns="http://data.altinkaynak.com/">'+
+//       '<Username>'+username+'</Username>'+
+//       '<Password>'+password+'</Password>'+
+//     '</AuthHeader>'+
+//   '</soap12:Header>'+
+//   '<soap12:Body>'+
+//    '<GetGold xmlns="http://data.altinkaynak.com/" />'+
+//   '</soap12:Body>'+
+// '</soap12:Envelope>';
 
 
-var xhttp = new XMLHttpRequest();
 
-var xhttpsoap = function (reqestMethod, Url) {
+var xhttpsoap = function (reqestMethod, Url,soapAction) {
+    var xhttp = new XMLHttpRequest();
+
     xhttp.open(reqestMethod, Url, true);
-    xhttp.setRequestHeader('Content-Type', 'application/soap+xml');
+    xhttp.setRequestHeader('Content-Type', 'text/xml; charset=utf-8');
+    xhttp.setRequestHeader('SOAPAction', soapAction);
     xhttp.send(soap);
     var data = "";
-    // xhttp.onreadystatechange = function () {
-    //     if (xhttp.readyState == 4) {
-    //         if (xhttp.status == 200) {
-    //             alert(xmlhttp.responseText);
-    //             data = xmlhttp.responseText;
-    //         }
-    //     }
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status == 200) {
+                alert(xmlhttp.responseText);
+                data = xmlhttp.responseText;
+            }
+        }
 
-    // }
-    xhttp.onload = function(){ alert (xhttp.responseText); } 
-    xhttp.onerror = function(){ alert (xhttp.responseText); }
+    }
+
 
 
 
@@ -68,6 +69,6 @@ var xhttpsoap = function (reqestMethod, Url) {
     return data;
 }
 var data = "";
-data = xhttpsoap(requesDataMethod, Url);
+data = xhttpsoap(requesDataMethod, Url,soapActionHeader);
 
 console.log(data);
